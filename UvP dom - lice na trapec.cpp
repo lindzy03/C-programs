f67d4d591895821ce14963709c6db4c070d123ab
lindzy03 <43601133+lindzy03@users.noreply.github.com>
2018-10-07 10:29:43 +0300

    add file
    dobavih programa za presmqtane lice na kvadrat

---
+++ UvP dom - lice na trapec.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{// lice na trapec
+    double b1,b2,h,Lice;
+    cin>>b1>>b2>>h;
+    Lice=((b1+b2)*h)/2;
+    cout<<"Lice="<<Lice;
+    return 0;
+}
