4c4d4f099a8026cfe417142d942656dbcdd7adc4
lindzy03 <43601133+lindzy03@users.noreply.github.com>
2018-10-07 20:21:14 +0300

    add file
    dobavih programa za prevrashtane inch v sm.

---
+++ UvP dom - inch v sm..cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{// incove v santimetri
+    double inch;
+    cin>>inch;
+    double sm=inch*2.54;
+    cout<<"Santimetri="<<sen;
+    return 0;
+}
