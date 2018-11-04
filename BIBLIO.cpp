72fca32822abd64602a0f2f0c0237885f739a48e
lindzy03 <43601133+lindzy03@users.noreply.github.com>
2018-11-04 21:28:52 +0200

    add new file
    add 12 reda

---
+++ BIBLIO.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+    int a=45,b=25,c=48;
+    cout<<"knigi="<<a/15<<endl;
+    int ceil (b/15);
+    cout<<"vestnici="<<b/15<<endl;
+    return 0;
+}
