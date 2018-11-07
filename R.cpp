852d4fe50723dc025374a0e0f5599655da87a145
lindzy03 <43601133+lindzy03@users.noreply.github.com>
2018-11-07 19:45:20 +0200

    add file
    add file

---
+++ R.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double h,m,t,kacane;
+    cin>>m;
+    cin>>m;
+    cin>>t;
+    cout<<"h"<<h<<endl;
+    cout<<"m"<<m<<endl;
+    cout<<"t"<<t<<endl;
+
+    return 0;
+
+}
