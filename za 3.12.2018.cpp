9caf457d83a53c00528924a0afd61b174f4096b7
lindzy03 <43601133+lindzy03@users.noreply.github.com>
2018-11-26 18:31:01 +0200

    add files
    add file

---
+++ za 3.12.2018.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    switch(x)
+    {
+        case 1:cout<<"I"<<endl;break;
+        case 5:cout<<"V"<<endl;break;
+        case 10:cout<<"X"<<endl;break;
+        case 50:cout<<"L"<<endl;break;
+        case 100:cout<<"C"<<endl;break;
+        case 500:cout<<"D"<<endl;break;
+        case 1000:cout<<"M"<<endl;break;
+        default:cout<<"Nqma savpadenie"<<endl;
+    }
+    return 0;
+}
